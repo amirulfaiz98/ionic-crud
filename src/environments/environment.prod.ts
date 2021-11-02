d471edf286d5e2821e4ba661c5b7282f74d68d88
Amirul Faiz <faizshamsul98@gmail.com>
2021-11-02 12:12:17 +0800

    Import and Register Firebase Services in Ionic

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyAGG6Dk_ZsGzwX-a_HN9yrOSZ1UvLI1kk8",
+    authDomain: "ionicfirebase-6d9fa.firebaseapp.com",
+    projectId: "ionicfirebase-6d9fa",
+    storageBucket: "ionicfirebase-6d9fa.appspot.com",
+    messagingSenderId: "670484251067",
+    appId: "1:670484251067:web:d77a86f4682a71fd0fc49e",
+    measurementId: "G-YH1Z5H96JL"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyAGG6Dk_ZsGzwX-a_HN9yrOSZ1UvLI1kk8",
+    authDomain: "ionicfirebase-6d9fa.firebaseapp.com",
+    projectId: "ionicfirebase-6d9fa",
+    storageBucket: "ionicfirebase-6d9fa.appspot.com",
+    messagingSenderId: "670484251067",
+    appId: "1:670484251067:web:d77a86f4682a71fd0fc49e",
+    measurementId: "G-YH1Z5H96JL"
+  }
 };
 
 /*
